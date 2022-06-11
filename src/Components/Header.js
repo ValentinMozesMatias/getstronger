39f07429119e1ca5460ad1026823193568692cf9
Mozerman <valentin.mozes@gmail.com>
2022-06-11 19:21:47 +0300

    sixth commit

---
+++ src/Components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header () {
+    return (
+        <div id='main'>
+            <div className='pr-heading'>
+                <h1><span>Get</span>Stronger</h1>
+                <a href="https://www.facebook.com/LiberiPeMunti">
+                <p className='details'>In partnership with "Liber pe Munti"</p>
+                </a>
+                <h2>Are you ready to explore?</h2>
+                <h2>Build your Phisique And Fitness With Profesionals</h2>
+                <div className='header-btns'></div>
+                    <a href='/#' className='header-btn'>JOIN US</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/JoinMentors.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import Header from './Header';
+
+//Pages
+import Mentors from './Mentors';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+function JoinMentors () {
+    return (
+        <Router>
+        <div id='mentors'>
+            <div className="pr-heading">
+                <h1>Our<span>{Mentors}</span></h1>
+                <p>Are you ready to explore?</p>
+                <a href="/Mentors" className='pr-btn' exact>Find a Mentor</a>
+                </div>
+        </div>
+        </Router>
+    )
+}
+
+export default JoinMentors;
